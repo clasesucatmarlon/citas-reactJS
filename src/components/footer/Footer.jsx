43ac591fe7654dfea5d6bb599ad609e30a64cbf0
clasesucatmarlon <clasesucatmarlon@gmail.com>
2022-03-17 20:24:34 -0500

    Arreglado responsive test 4

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,42 +4,43 @@ import { faLinkedinIn, faGithubAlt, faTwitter } from '@fortawesome/free-brands-s
 
 const Footer = () => {
     return (
-        <div className='p-4 mt-12 border-t-4 border-gray-200 bg-red-500 md:w-full '>
+        <div className='p-4 mt-12 border-t-4 border-gray-200 bg-red-500 md:w-full flex flex-col items-center '>
             <div className='flex flex-col items-center ' >
                 <h3>Diseño y desarrollo por {''}
                     <span className='italic'>Marlon García</span>
                 </h3>
                 <h3>Medellín 2022</h3>
-                <div className="flex justify-between p-4 items-center">
-                    <a 
-                        href="https://clasesucatmarlon.github.io/portafolioMG/" 
-                        className="hover:text-indigo-700"
-                        target="_blank"
-                        >
-                        <FontAwesomeIcon className='px-4 text-2xl' icon={faLink} />
-                    </a>
-                    <a 
-                        href="https://www.linkedin.com/in/marlonagarciam/" 
-                        className="hover:text-indigo-700"
-                        target="_blank"
-                        >
-                        <FontAwesomeIcon className='px-4 text-2xl' icon={faLinkedinIn} />
-                    </a>
-                    <a 
-                        href="https://github.com/clasesucatmarlon" 
-                        className="hover:text-indigo-700"
-                        target="_blank"
-                    >
-                        <FontAwesomeIcon className='px-4 text-2xl' icon={faGithubAlt} />
-                    </a>
-                    <a 
-                        href="https://twitter.com/garciamarlon" 
-                        className="hover:text-indigo-700"
-                        target="_blank"
-                    >
-                        <FontAwesomeIcon className='px-4 text-2xl' icon={faTwitter} />
-                    </a>
-                </div>
+            </div>
+
+            <div className="flex justify-between p-4 items-center">
+                <a
+                    href="https://clasesucatmarlon.github.io/portafolioMG/"
+                    className="hover:text-indigo-700"
+                    target="_blank"
+                >
+                    <FontAwesomeIcon className='px-4 text-2xl' icon={faLink} />
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/marlonagarciam/"
+                    className="hover:text-indigo-700"
+                    target="_blank"
+                >
+                    <FontAwesomeIcon className='px-4 text-2xl' icon={faLinkedinIn} />
+                </a>
+                <a
+                    href="https://github.com/clasesucatmarlon"
+                    className="hover:text-indigo-700"
+                    target="_blank"
+                >
+                    <FontAwesomeIcon className='px-4 text-2xl' icon={faGithubAlt} />
+                </a>
+                <a
+                    href="https://twitter.com/garciamarlon"
+                    className="hover:text-indigo-700"
+                    target="_blank"
+                >
+                    <FontAwesomeIcon className='px-4 text-2xl' icon={faTwitter} />
+                </a>
             </div>
         </div>
 
